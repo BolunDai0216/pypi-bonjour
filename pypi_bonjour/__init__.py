a4887d734e526e6cf351a8e33031bb1b6c9fd422
Bolun Dai <bolun@faunarobotics.com>
2024-08-15 17:08:27 -0400

    updated github actions to build manylinux wheels

--- pypi_bonjour/__init__.py
+++ pypi_bonjour/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for pypi-bonjour."""
 
-__author__ = """Bolun Dai"""
+__author__ = "Bolun Dai"
 __email__ = "daibolun19950216@gmail.com"
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 from .common_cpp import greet
