a7be5cd3fc39bd7efb82be929f30c722414d7bc7
Bolun Dai <bolun@faunarobotics.com>
2024-08-16 13:18:44 -0400

    added dependency installation

--- scripts/before_build.sh
+++ scripts/before_build.sh
@@ -2,9 +2,15 @@
 
 set -e
 
+# upgrade pip
+python -m pip install -U pip
+
 # remove caches
 rm -rf build/
 rm -rf /tmp/cibuildwheel/
 
 # install pybind11
 pip install "pybind11[global]"
+
+# install dependencies
+pip install cmeel-assimp cmeel-octomap cmeel-qhull pin[build]
