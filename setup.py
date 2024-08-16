642e3687fcd43bae8810f4880a7b6332b4a95604
Bolun Dai <bolun@faunarobotics.com>
2024-08-16 16:02:51 -0400

    updated setup.py to match the pybind11 example

--- setup.py
+++ setup.py
@@ -7,6 +7,14 @@ from pathlib import Path
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
+# Convert distutils Windows platform specifiers to CMake -A arguments
+PLAT_TO_CMAKE = {
+    "win32": "Win32",
+    "win-amd64": "x64",
+    "win-arm32": "ARM",
+    "win-arm64": "ARM64",
+}
+
 
 # A CMakeExtension needs a sourcedir instead of a file list.
 # The name must be the _single_ output extension from the CMake build.
